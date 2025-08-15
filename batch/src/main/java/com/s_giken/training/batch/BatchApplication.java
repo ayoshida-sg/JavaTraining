e594cef5a942f50c82ab9250b4eadb5acfd8c6b3
yoshida-arisa <yoshida-arisa@PC191.s-giken.local>
2025-08-15 16:49:28 +0900

    対象年月の設定

--- batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
+++ batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
@@ -1,5 +1,10 @@
 package com.s_giken.training.batch;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -38,8 +43,25 @@ public class BatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws RuntimeException {
 		logger.info("-".repeat(40));
-
-		// TODO: ここにバッチ処理のコードを記述する
+		
+		
+		if (args.length != 1) {
+			throw new IllegalArgumentException("コマンドライン引数が不正です。対象年月を1つだけ指定してください。");
+		}
+			
+		String input = args[0];
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+		
+		try {
+			YearMonth yearMonth = YearMonth.parse(input, formatter);
+			LocalDate targetDate = yearMonth.atDay(1);
+			System.out.println("対象年月:" + targetDate);
+		} catch (DateTimeParseException e) {
+			throw new IllegalArgumentException("コマンドライン引数の書式が不正です。対象年月はyyyyMM形式で指定してください。");
+		}
+	}
+		
+    	// TODO: ここにバッチ処理のコードを記述する
 		// - データベースからデータを取得する
 		// - データを加工する
 		// - 加工したデータをデータベースに登録する
